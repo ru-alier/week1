1bf1c5c107aa2153b7cc06f51343025423a15994
ru-alier <rozpadyuk.a@gmail.com>
2020-11-09 19:33:11 +0700

    + result4

---
+++ public_html/result4/index.js
@@ -0,0 +1,23 @@
+import { Server } from 'http';
+import 'fast-json-stable-stringify';
+const s = Server((req, res) => {
+    let data = '';
+    res.writeHead(200, {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+        'Access-Control-Allow-Headers': 'x-test,Content-Type,Accept,Access-Control-Allow-Headers',
+        'Content-Type': 'application/json'});
+    debugger;
+    req
+    .on('data', chunk => {
+        data+=chunk;
+    })
+    .on('end', ()=>
+    res.end(JSON.stringify({
+        "message" : "rozpadyuka",
+    "x-result" : req.headers["x-test"],
+    "x-body" : data
+    }))
+    );
+});
+s.listen(4321, ()=>console.log(process.pid));
